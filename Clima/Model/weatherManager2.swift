3cd0aa84296c1e91406873bfe88989efcc632e73
Kimminjong94 <79985912+Kimminjong94@users.noreply.github.com>
2021-10-28 17:41:10 +0900

    Update weatherManager2.swift
    changed because of token exposure

--- Clima/Model/weatherManager2.swift
+++ Clima/Model/weatherManager2.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid=f0c0f1d863f4c9dece58a3ed389503e9"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&appid="
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
